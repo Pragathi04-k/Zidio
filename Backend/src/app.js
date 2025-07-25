0781a766eaf646c4ba1abf44204b12c17b6a7af9
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2025-07-25 12:29:15 +0530

    Enhance UI

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,9 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://zidio-bay.vercel.app',
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
